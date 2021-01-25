7ff8647f02b2ac6fc3dcdd31377c46a8ea35fb1d
Nikitala0014 <50992433+Nikitala0014@users.noreply.github.com>
2021-01-25 13:59:00 +0700

    Update main.ts

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -2,7 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
+// 1
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
+  
+// 2
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({
+//             origin: true,
+//             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//             allowedHeaders: ['Content-Type', 'Authorization']
+//             credentials: true,
+//         });
+//   await app.listen(3000);
 }
 bootstrap();
